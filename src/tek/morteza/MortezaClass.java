e717e3259b1494926fc7590e890b71486e222ed4
Morteza <morteza.sharifi.1995@gmail.com>
2023-05-17 21:32:55 -0400

    Changing

--- src/tek/morteza/MortezaClass.java
+++ src/tek/morteza/MortezaClass.java
@@ -3,6 +3,8 @@ package tek.morteza;
 public class MortezaClass {
 	public static void main(String[] args) {
 		System.out.println("This is Morteza. Tek school student");
+		
+		System.out.println("SOmechanging after branching");
 	}
 
 }
